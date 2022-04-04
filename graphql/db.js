38baf760a7b22b2ea5048c3a95af0e28370e2827
bopboy <39391891+bopboy@users.noreply.github.com>
2022-04-05 05:02:40 +0900

    09 스키마 연습 5 (mutation)

--- graphql/db.js
+++ graphql/db.js
@@ -34,3 +34,11 @@ export const deleteMovie = id => {
         return false
     }
 }
+export const addMovie = (name, score) => {
+    const newMovie = {
+        id: movies.length + 1,
+        name, score
+    }
+    movies.push(newMovie)
+    return newMovie
+}
