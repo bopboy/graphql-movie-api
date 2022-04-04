4bc8621b412c34d36d79e23d25b96db29b550f71
bopboy <39391891+bopboy@users.noreply.github.com>
2022-04-05 04:17:09 +0900

    07 스키마 연습 (인자 전달)

--- graphql/db.js
+++ graphql/db.js
@@ -1,15 +1,18 @@
 export const people = [
     {
+        id: '0',
         name: 'bopboy',
         age: 18,
         gender: "male"
     },
     {
+        id: '1',
         name: 'james',
         age: 20,
         gender: "male"
     },
     {
+        id: '2',
         name: 'julie',
         age: 25,
         gender: "female"
@@ -18,6 +21,6 @@ export const people = [
 ]
 
 export const getById = id => {
-    const filteredPeople = people.filter(person => person.id === id)
+    const filteredPeople = people.filter(person => person.id === id + "")
     return filteredPeople[0]
 }
