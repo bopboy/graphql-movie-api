6b2b8d3e0a2c7d884ba897f815baf8573a93dd68
bopboy <39391891+bopboy@users.noreply.github.com>
2022-04-05 04:37:47 +0900

    08 스키마 연습 4

--- graphql/db.js
+++ graphql/db.js
@@ -1,26 +1,36 @@
-export const people = [
+const movies = [
     {
-        id: '0',
-        name: 'bopboy',
-        age: 18,
-        gender: "male"
+        id: 0,
+        name: 'Alien',
+        score: 9.5
     },
     {
-        id: '1',
-        name: 'james',
-        age: 20,
-        gender: "male"
+        id: 1,
+        name: 'Matrix',
+        score: 9.2
     },
     {
-        id: '2',
-        name: 'julie',
-        age: 25,
-        gender: "female"
+        id: 2,
+        name: 'Star Wars',
+        score: 8.2,
+    },
+    {
+        id: 3,
+        name: 'The Lord of the Rings',
+        score: 7.5
     }
-
 ]
-
+export const getMovies = () => movies
 export const getById = id => {
-    const filteredPeople = people.filter(person => person.id === id + "")
-    return filteredPeople[0]
+    const filteredMovies = movies.filter(movie => movie.id === id)
+    return filteredMovies[0]
+}
+export const deleteMovie = id => {
+    const cleanedMovies = movies.filter(movie => movie.id !== id)
+    if (movies.length > cleanedMovies.length) {
+        movies = cleanedMovies
+        return true
+    } else {
+        return false
+    }
 }
