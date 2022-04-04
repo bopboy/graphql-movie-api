7976e36cd25289d1810f0253fbdca872ac14b961
bopboy <39391891+bopboy@users.noreply.github.com>
2022-04-05 05:37:09 +0900

    12 graphql 로 rest api 래핑

--- graphql/db.js
+++ graphql/db.js
@@ -1,44 +1,9 @@
-let movies = [
-    {
-        id: 0,
-        name: 'Alien',
-        score: 9.5
-    },
-    {
-        id: 1,
-        name: 'Matrix',
-        score: 9.2
-    },
-    {
-        id: 2,
-        name: 'Star Wars',
-        score: 8.2,
-    },
-    {
-        id: 3,
-        name: 'The Lord of the Rings',
-        score: 7.5
-    }
-]
-export const getMovies = () => movies
-export const getById = id => {
-    const filteredMovies = movies.filter(movie => movie.id === id)
-    return filteredMovies[0]
-}
-export const deleteMovie = id => {
-    const cleanedMovies = movies.filter(movie => movie.id !== id)
-    if (movies.length > cleanedMovies.length) {
-        movies = cleanedMovies
-        return true
-    } else {
-        return false
-    }
-}
-export const addMovie = (name, score) => {
-    const newMovie = {
-        id: movies.length + 1,
-        name, score
-    }
-    movies.push(newMovie)
-    return newMovie
+import fetch from "node-fetch"
+const API_URL = "https://yts.am/api/v2/list_movies.json?"
+
+export const getMovies = (limit, rating) => {
+    let REQ_URL = API_URL
+    if (limit > 0) REQ_URL += `limit=${limit}`
+    if (rating > 0) REQ_URL += `&minimum_rating=${rating}`
+    return fetch(`${REQ_URL}`).then(res => res.json()).then(json => json.data.movies)
 }
